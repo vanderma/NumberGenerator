4223156fcb7417c7ea136c7e71309aeb3059356b
Valentin Andermatt <valentin.andermatt@gmx.ch>
2018-09-18 21:38:02 +0200

    Surround Text

--- src/ch/vanderma/ConsoleOutput.java
+++ src/ch/vanderma/ConsoleOutput.java
@@ -2,6 +2,8 @@ package ch.vanderma;
 
 public class ConsoleOutput {
     public void handleNumberChanged(int newNumber) {
+        System.out.println("------------------------------------------------------");
         System.out.println("Number: " + newNumber);
+        System.out.println("------------------------------------------------------");
     }
 }
